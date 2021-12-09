e5b162058821167cfb1c3e0851e7ad69a4668944
SalvadorDuenas <89178506+SalvadorDuenas@users.noreply.github.com>
2021-12-09 15:12:40 -0600

    Add files via upload

---
+++ Hilos/src/main/java/com/mycompany/hilos/HelloRunnable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hilos;
+
+public class HelloRunnable implements Runnable {
+
+    public void run() {
+        System.out.println("Hello from a thread!");  
+    }
+}
+//HelloRunnable.java
---
+++ Hilos/src/main/java/com/mycompany/hilos/principal.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.hilos;
+
+public class principal {
+
+    public static void main(String[] args) {
+
+        //Impares con herencia
+        (new impares()).start();
+     
+        
+    }
+}
